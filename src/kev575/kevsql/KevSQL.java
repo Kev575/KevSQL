84f2dddee10e67b58d8560aa3e62a6dd7bea0afe
Kev575 <phoenix.games.minecraft@gmail.com>
2016-10-24 17:46:57 +0200

    Comments Update and Dedicated Connections

--- src/kev575/kevsql/KevSQL.java
+++ src/kev575/kevsql/KevSQL.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import kev575.kevsql.connection.KevSQLConnection;
 import kev575.kevsql.connection.SQLConnection;
 
+/**
+ * the KevSQL main class
+ * @author Kev575
+ */
 public final class KevSQL {
 
 	private static final HashMap<String, SQLConnection> connection = new HashMap<>(); 
@@ -15,7 +19,7 @@ public final class KevSQL {
 	/**
 	 * Create a connection
 	 * @param name
-	 * @return the created {@link SQLConnection}<br>It's in version 0.1 an instance of {@link KevSQLConnection}.
+	 * @return the created {@link SQLConnection}<br>version 0.1 and above: instance of {@link KevSQLConnection}
 	 */
 	public static SQLConnection createConnection(String name) {
 		if (connection.containsKey(name))
@@ -23,6 +27,16 @@ public final class KevSQL {
 		connection.put(name, new KevSQLConnection() {
 		});
 		return connection.get(name);
+	}
+	
+	/**
+	 * Create a dedicated connection without a name<br>You can also construct the connection using <code><blockquote>new KevSQLConnection()</blockquote></code>
+	 * <u><b>Why you shouldn't use the constructor version</b></u><br><blockquote>The KevSQLConnection is now the latest (and greatest) connection type of KevSQL, but this could change in some versions to expand
+	 * the functions or other things. Besides that you might consider using SQLConnection as variable type to avoid this kind of issue.</blockquote>
+	 * @return a new {@link SQLConnection}<br>version 0.1 and above: instance of {@link KevSQLConnection}
+	 */
+	public static SQLConnection createDedicatedConnection() {
+		return new KevSQLConnection();
 	}
 	
 	/**
